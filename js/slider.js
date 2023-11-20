c38db77277f63f1fea1460b94643a793e4d46250
manishinnoraft <manish.vijay@innoraft.com>
2023-11-20 12:05:19 +0530

    Slider Issue Fixed

--- js/slider.js
+++ js/slider.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
   });
 });
 
-$(function () {
-  $(".responsive").slick({
+$(function() {
+  $('.responsive').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 3,
     adaptiveHeight: true,
@@ -22,24 +22,24 @@ $(function () {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots:true,
-        },
+          dots: true,
+
+        }
       },
       {
         breakpoint: 992,
-
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-        },
+        }
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-        },
-      },
-    ],
+        }
+      }
+    ]
   });
 });
