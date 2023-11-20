8fd4e141a6a82511f7cca53133719b555e4372db
manishinnoraft <manish.vijay@innoraft.com>
2023-11-20 10:34:34 +0530

    Pushing the updated changes

---
+++ js/slider.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  const menuBtn = $(".bar_icon");
+  const list = $(".navbar__list");
+  menuBtn.click(function () {
+    list.toggle();
+  });
+});
+
+$(function () {
+  $(".responsive").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots:true,
+        },
+      },
+      {
+        breakpoint: 992,
+
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
